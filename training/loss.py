7f555412d6348155552bc07bb6f16e9e6df676e2
Yuma Kishi <obake2ai@gmail.com>
2021-02-02 14:10:08 +0900

    debug

--- training/loss.py
+++ training/loss.py
@@ -182,7 +182,7 @@ class StyleGAN2Loss_obake(Loss): #this func is called by default
         return logits
 
     def run_D_face(self, img):
-        numpy_img = img.to('cpu').detach().numpy().copy().transpose(0, 2, 3, 1)
+        numpy_img = img.to('cpu').detach().numpy().copy().transpose(0, 2, 3, 1)*255
         logits = 0
         for idx in range(numpy_img.shape[0]):
             pil_img=torchvision.transforms.functional.to_pil_image(numpy_img[idx], mode='RGB')
