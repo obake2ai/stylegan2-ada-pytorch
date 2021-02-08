85be00e0b58adf86e951790d02712f5fd5a87f89
Yuma Kishi <obake2ai@gmail.com>
2021-02-08 11:40:34 +0900

    debug

--- training/training_loop.py
+++ training/training_loop.py
@@ -295,6 +295,7 @@ def training_loop(
             for round_idx, (real_img, real_c, gen_z, gen_c) in enumerate(zip(phase_real_img, phase_real_c, phase_gen_z, phase_gen_c)):
                 sync = (round_idx == batch_size // (batch_gpu * num_gpus) - 1)
                 gain = phase.interval
+                print (phase.name)
                 loss.accumulate_gradients(phase=phase.name, real_img=real_img, real_c=real_c, gen_z=gen_z, gen_c=gen_c, sync=sync, gain=gain)
 
             # Update weights.
